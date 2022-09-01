bb0111a802ea2e6834f36b71dce6a7713b6c44e0
Nishanth Kumar Pathi <pathinishanth@gmail.com>
2022-09-01 14:14:45 +0200

    made error

--- dev/c2-dev-bucket.tf
+++ dev/c2-dev-bucket.tf
@@ -3,7 +3,8 @@ module "creatingdevbucket" {
     
     m_bucket_name = "apple"
     m_bucket_location = "us-east1"
-    m_environment_name = "dev"
+    //m_environment_name = "dev"
+    m_environment_name = "de
 }
 
 # All Three Branch Code
